1ac95570a6945d4184b34a84d89bac6efa6f037c
Lam4w <nguyentunglam4w@gmail.com>
2025-09-13 16:58:54 +0700

    update

---
+++ SceneDelegate.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, ObservableObject {
+    @Published var route: UniversalLinkManager.Route = .index
+
+    func scene(_ scene: UIScene, willConnectTo
+               session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        print("connectionOptions.userActivities: \(connectionOptions.userActivities)")
+        guard let userActivity = connectionOptions.userActivities.first,
+            userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+            let incomingURL = userActivity.webpageURL else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.route = UniversalLinkManager.handleUniversalLink(url: incomingURL)
+        }
+    }
+    
+    func onContinueBrowsingWebUserActivity(url: URL) {
+        DispatchQueue.main.async {
+            self.route = UniversalLinkManager.handleUniversalLink(url: url)
+        }
+
+    }
+
+}
